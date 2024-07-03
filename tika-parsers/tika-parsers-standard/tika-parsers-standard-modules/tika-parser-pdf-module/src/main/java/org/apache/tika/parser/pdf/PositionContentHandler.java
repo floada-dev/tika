631ec239b64f7d20b9b5bc2898caa52adfae7766
Henrik Johansson <henrik@floada.com>
2024-07-03 08:23:38 +0200

    Added paragraph aware position content handler as an option.

--- tika-parsers/tika-parsers-standard/tika-parsers-standard-modules/tika-parser-pdf-module/src/main/java/org/apache/tika/parser/pdf/PositionContentHandler.java
+++ tika-parsers/tika-parsers-standard/tika-parsers-standard-modules/tika-parser-pdf-module/src/main/java/org/apache/tika/parser/pdf/PositionContentHandler.java
@@ -16,30 +16,94 @@
  */
 package org.apache.tika.parser.pdf;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.apache.pdfbox.text.TextPosition;
+import org.apache.pdfbox.util.Matrix;
+import org.apache.tika.sax.ContentHandlerDecorator;
 import org.xml.sax.ContentHandler;
 
-import org.apache.tika.sax.ContentHandlerDecorator;
+import java.util.List;
 
+public abstract class PositionContentHandler extends ContentHandlerDecorator {
 
-public class PositionContentHandler extends ContentHandlerDecorator {
+    private static final char NL = '\n';
+    private static final char WS = ' ';
 
-    private final Map<Integer, List<TextPosition>> textPositions = new LinkedHashMap<>();
-
-    public PositionContentHandler(ContentHandler contentHandler) {
-        super(contentHandler);
+    PositionContentHandler(ContentHandler handler) {
+        super(handler);
     }
 
-    public Map<Integer, List<TextPosition>> getTextPositions() {
-        return textPositions;
+    void nextParagraph() {
+
     }
 
-    public void addPositions(Integer page, List<TextPosition> positions) {
-        this.textPositions.computeIfAbsent(page, p -> new ArrayList<>()).addAll(positions);
+    void endParagraph() {
+
     }
+
+    void addLineSeparator() {
+
+    }
+
+    void addNewline() {
+        addCharacter(NL, false);
+    }
+
+    void addWhitespace() {
+        addCharacter(WS, true);
+    }
+
+    private void addCharacter(char character, boolean skipRepeats) {
+        List<TextPosition> textPositions = getLastTextPositions();
+
+        if (textPositions == null || textPositions.isEmpty()) {
+            return;
+        }
+
+        TextPosition last = textPositions.get(textPositions.size() - 1);
+        if (character == NL && " ".equals(last.getUnicode())) {
+            textPositions.remove(last);
+            if (textPositions.isEmpty()) {
+                return;
+            }
+
+            last = textPositions.get(textPositions.size() - 1);
+        }
+
+        String stringChar = String.valueOf(character);
+        if (skipRepeats && stringChar.equals(last.getUnicode())) {
+            return;
+        }
+
+        TextPosition textPos = buildTextPosition(character, last);
+        textPositions.add(textPos);
+    }
+
+    private static TextPosition buildTextPosition(char character, TextPosition last) {
+        Matrix textMatrix = last.getTextMatrix();
+        // Set start x to endX of last char
+        textMatrix.setValue(2, 0, last.getEndX());
+
+        int[] charCodes = new int[]{character};
+
+        return new TextPosition(
+                last.getRotation(),
+                last.getPageWidth(),
+                last.getPageHeight(),
+                textMatrix,
+                last.getEndX() + 0.01f,
+                last.getEndY(),
+                last.getHeight(),
+                last.getWidth(),
+                last.getWidthOfSpace(),
+                String.valueOf(character),
+                charCodes,
+                last.getFont(),
+                last.getFontSize(),
+                Math.round(last.getFontSizeInPt())
+        );
+    }
+
+    abstract void nextPage();
+    abstract void addPositions(List<TextPosition> positions);
+    abstract List<TextPosition> getLastTextPositions();
 }
